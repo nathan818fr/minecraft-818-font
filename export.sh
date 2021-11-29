2990e53d0fe349edcb34975f202b524ef12cccad
Nathan Poirier <nathan@poirier.io>
2021-11-29 20:16:36 +0100

    Initial version

---
+++ export.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -Eeuo pipefail
+shopt -s inherit_errexit
+
+function export_resourcepack() {
+  local scale tmpdir
+  scale="$1"
+  tmpdir="$(mktemp -d)"
+
+  # Prepare content
+  cat './src/pack.mcmeta' | sed "s/%%SCALE%%/${scale}/g" | sed "s/%%VERSION%%/${version}/g" >"${tmpdir}/pack.mcmeta"
+  mkdir -p "${tmpdir}/assets/minecraft/textures/font"
+  cp './src/ascii.properties' "${tmpdir}/assets/minecraft/textures/font/ascii.properties"
+  cp "./bitmap/ascii_${scale}.png" "${tmpdir}/assets/minecraft/textures/font/ascii.png"
+
+  # Create zip
+  zipfile="$(realpath -m "./out/818_HD_Font_${scale}.zip")"
+  rm -f "$zipfile"
+  mkdir -p "$(dirname "$zipfile")"
+  (cd "$tmpdir" && zip -r "$zipfile" ./*)
+}
+
+function main() {
+  version="0.1"
+  for scale in x{1..8}; do
+    export_resourcepack "$scale"
+  done
+}
+
+main "$@"
+exit 0
