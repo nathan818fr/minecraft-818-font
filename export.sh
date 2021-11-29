601f60518f2a8f1a53973174d0a7b10c22c32f16
Nathan Poirier <nathan@poirier.io>
2021-11-29 22:47:37 +0100

    Add pack thumbnail

--- export.sh
+++ export.sh
@@ -9,6 +9,8 @@ function export_resourcepack() {
 
   # Prepare content
   cat './src/pack.mcmeta' | sed "s/%%SCALE%%/${scale}/g" | sed "s/%%VERSION%%/${version}/g" >"${tmpdir}/pack.mcmeta"
+  convert -font Consolas -fill white -pointsize 60 -gravity SouthEast -draw "text 0,-10 '${scale}'" \
+    './src/pack.png' "${tmpdir}/pack.png"
   mkdir -p "${tmpdir}/assets/minecraft/textures/font"
   cp './src/ascii.properties' "${tmpdir}/assets/minecraft/textures/font/ascii.properties"
   cp "./bitmap/ascii_${scale}.png" "${tmpdir}/assets/minecraft/textures/font/ascii.png"
